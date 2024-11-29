524025c76ef69bff088474577b96978668795c1a
Victor Bourgin <victor.bourgin1@universite-paris-saclay.fr>
2024-11-29 11:36:04 +0100

    First implementation of checks

---
+++ checks.py
@@ -0,0 +1,38 @@
+from postproc.output_checks import atmo_surf_mass_balance, number_mass_consistency
+
+from netCDF4 import Dataset
+
+
+def main():
+    # Parameter
+    diameter = 2e-5
+    density = 2500
+    # Output files
+    fxy_path = './data/rnr_flat_test_depo_xy.006.nc'
+    f3d_path = './data/rnr_flat_test_depo_3d.006.nc'
+
+    # Load netCDF files
+    fxy = Dataset(fxy_path, 'r+')
+    f3d = Dataset(f3d_path, 'r+')
+
+    # Load dimensions
+    x, y = f3d.variables['x'][:], f3d.variables['y'][:]
+    zu_3d, zw_3d = f3d.variables['zu_3d'][:], f3d.variables['zw_3d'][:]
+    time = f3d.variables['time'][1:]
+
+    # Load variables
+    mconc1 = f3d.variables['dust_mc_bin1'][:]
+    mconc2 = f3d.variables['dust_mc_bin2'][:]
+
+    surf_mass1 = fxy.variables['dust_surf_mass_bin1_xy'][:]
+    surf_mass2 = fxy.variables['dust_surf_mass_bin2_xy'][:]
+
+    surf_numb1 = fxy.variables['dust_surf_numb_bin1_xy'][:]
+    surf_numb2 = fxy.variables['dust_surf_numb_bin2_xy'][:]
+
+    atmo_surf_mass_balance(time, x, y, zw_3d, mconc2, surf_mass2)
+
+    number_mass_consistency(time, surf_mass2, surf_numb2, diameter, density)
+
+if __name__ == '__main__':
+    main()
---
+++ postproc/output_checks.py
@@ -0,0 +1,81 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def cells_volume(x, y, zw_3d):
+    # Compute cell sizes in all directions
+    dx = x[1] - x[0]
+    dy = y[1] - y[0]
+    dz = zw_3d[1:] - zw_3d[:-1]
+
+    return dx*dy*dz
+
+def atmo_surf_mass_balance(time, x, y, zw_3d, mconc, surf_mass):
+    # Initialze arrays
+    atmo_mass = np.zeros_like(time)
+    depo_mass = np.zeros_like(time)
+
+    # Compute total suspended dust mass
+    vols = cells_volume(x, y, zw_3d)
+
+    for t in range(len(time)):
+        atmo_mass[t] = np.sum(np.sum(mconc[t,1:,:,:], axis=(1,2)) * vols) * 1000
+        depo_mass[t] = np.sum(surf_mass[t,0,:,:]) * 1000
+
+    tot_mass = atmo_mass + depo_mass
+
+    # Estimate error
+    abs_error = tot_mass[-1] - tot_mass[0]
+    rel_error = abs(abs_error)/tot_mass[0]*100
+
+    print(f'absolute error: {abs_error:.2e} g')
+    print(f'relative error: {rel_error:.2e} %')
+
+    # Plot mass evolution in the domain
+    fig, ax = plt.subplots()
+
+    ax.plot(time, atmo_mass, color='r', ls='--', label='Suspended')
+    ax.plot(time, depo_mass, color='r', label='Deposited')
+    ax.plot(time, tot_mass, color='k', label='Total')
+
+    ax.set_xlim([time[0], time[-1]])
+    ax.set_ylim([0.0, 1.1*atmo_mass.max()])
+
+    ax.grid(True)
+
+    ax.set_xlabel('Time [s]')
+    ax.set_ylabel('Mass [g]')
+    ax.legend()
+
+    fig.savefig('./figs/mass_balance.png', dpi=300)
+
+
+def number_mass_consistency(time, surf_mass, surf_numb, diameter, density):
+    # Initialiaze arrays
+    mass_tot = np.zeros_like(time)
+    mass_from_num = np.zeros_like(time)
+
+    # Mass of one particle
+    part_mass = (np.pi / 6) * (diameter**3) * density
+
+    # Compute total particle mass and number
+    for t in range(len(time)):
+        mass_tot[t] = np.sum(surf_mass[t,0,:,:]) * 1000
+        mass_from_num[t] = np.sum(surf_numb[t,0,:,:]) * part_mass * 1000
+
+    # Plot evolution over time
+    fig, ax = plt.subplots()
+
+    ax.plot(time, mass_tot, color='k', label='Total mass')
+    ax.plot(time, mass_from_num, color='k', ls='--', label='Mass from numb.')
+
+    ax.set_xlim([time[0], time[-1]])
+    ax.set_ylim([0.0, 1.1*mass_tot.max()])
+
+    ax.grid(True)
+
+    ax.set_xlabel('Time [s]')
+    ax.set_ylabel('Mass [g]')
+    ax.legend()
+
+    fig.savefig('./figs/mass_consistency.png', dpi=300)
